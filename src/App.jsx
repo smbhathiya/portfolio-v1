96d3cd61f25a56e7afdad1f1d052b9ff9b654710
smbhathiya <smbhathiya0@gmail.com>
2024-07-26 22:59:50 +0530

    aos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
